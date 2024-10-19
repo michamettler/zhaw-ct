44b5812972be91971ff14c08a9c4d25298f675f1
mettlmi1 <mettlermicha@gmail.com>
2024-10-19 11:42:50 +0200

    finished L05 part 1

--- Lab05_ArithemticOperations/sum_diff/app/sumdiff.s
+++ Lab05_ArithemticOperations/sum_diff/app/sumdiff.s
@@ -37,9 +37,44 @@ main    PROC
 
 user_prog
         ; STUDENTS: To be programmed
+		
 
-
-
+		; read & store operand A (R1) and operand B (R2) from dip switches
+		
+		; operand A
+		LDR R0, =ADDR_DIP_SWITCH_15_8
+		LDRB R1, [R0]
+		LSLS R1, R1, #24
+		;operand B
+		LDR R0, =ADDR_DIP_SWITCH_7_0
+		LDRB R2, [R0]
+		LSLS R2, R2, #24
+		
+		
+		; execute and display operations
+		
+		; addition (R3) & show on leds
+		ADDS R3, R1, R2
+		MRS R5, APSR
+		LSRS R3, R3, #24
+		LDR R0, =ADDR_LED_7_0
+		STRB R3, [R0]
+		; flags addition (R5)
+		LSRS R5, R5, #24
+		LDR R0, =ADDR_LED_15_8
+		STRB R5, [R0]
+		
+		; subtraction (R4) & show on leds
+		SUBS R4, R1, R2
+		MRS R6, APSR
+		LSRS R4, R4, #24
+		LDR R0, =ADDR_LED_23_16
+		STRB R4, [R0]
+		; flags subtraction (R6)
+		LSRS R6, R6, #24
+		LDR R0, =ADDR_LED_31_24
+		STRB R6, [R0]
+		
 
         ; END: To be programmed
         B       user_prog
