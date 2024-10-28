d192cf33c1d0612eedac9b35263c2d92e1615801
mettlmi1 <mettlermicha@gmail.com>
2024-10-28 20:07:38 +0100

    finished lab6 part 1

--- Lab06_ALUBranchInstructions/bcd/app/main.s
+++ Lab06_ALUBranchInstructions/bcd/app/main.s
@@ -21,7 +21,8 @@
 ; -- Address Defines
 ; ------------------------------------------------------------------
 
-ADDR_LED_15_0           EQU     0x60000100
+ADDR_LED_7_0            EQU     0x60000100
+ADDR_LED_15_8           EQU     0x60000101
 ADDR_LED_31_16          EQU     0x60000102
 ADDR_DIP_SWITCH_7_0     EQU     0x60000200
 ADDR_DIP_SWITCH_15_8    EQU     0x60000201
@@ -46,8 +47,75 @@ main    PROC
             
 ; STUDENTS: To be programmed
 
+		LDR R3,=0xF0
+		
+		; BCD tens to R1
+		LDR R0, =ADDR_DIP_SWITCH_15_8
+		LDRB R1, [R0]
+		BICS R1, R1, R3
+		
+		; BCD ones to R2
+		LDR R0, =ADDR_DIP_SWITCH_7_0
+		LDRB R2, [R0]
+		BICS R2, R2, R3
+		
+		; Write BCD to LED
+		LDR R0, =ADDR_LED_7_0
+		LSLS R4, R1, #4
+		MOVS R5, R2
+		ORRS R5, R5, R4
+		STR R5, [R0]
+		
+		; check if T0 is pressed
+		LDR		R0,=ADDR_BUTTONS
+		LDR		R6,=0x01
+		LDRB	R0,[R0]
+		TST		R0, R6		; check if t0 is pressed
+		BEQ		mult		; true
+		B shift				; false
+		
+		; -- something else is being executed --
+		B main
+		
+shift
+		; multiplication with shift
+		MOVS R6, R1
+		LSLS R6, R6, #3
+		ADDS R6, R6, R1
+		ADDS R6, R6, R1
+		ADDS R6, R6, R2
+		; color red
+		LDR R7, =ADDR_LCD_RED
+		LDR R0, =0x0000
+		STRH R0, [R7, #4]
+		LDR R0, =0xffff
+		STRH R0, [R7, #0]
+		B display
+		
+mult
+		; multiplication with multiplication
+		MOVS R6, R1
+		MOVS R0, #10
+		MULS R6, R0, R6
+		ADDS R6, R6, R2
+		; color red
+		LDR R7, =ADDR_LCD_RED
+		LDR R0, =0x0000
+		STRH R0, [R7, #0]
+		LDR R0, =0xffff
+		STRH R0, [R7, #4]
+		B display
 
-
+display
+		;write binary to LED
+		LDR R0, =ADDR_LED_15_8
+		STRB R6, [R0]
+		
+		; write result to 7 segment
+		LDR  R0, =ADDR_7_SEG_BIN_DS3_0
+		STRB R6, [R0, #1]  
+		STRB R5, [R0, #0]
+		B main
 
 ; END: To be programmed
 
---
+++ Lab06_ALUBranchInstructions/bcd/build/system_ctboard.d
@@ -0,0 +1,10 @@
+./build/system_ctboard.o: RTE\Device\CT_Board_HS14_M0\system_ctboard.c \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\Device\Include\system_ctboard.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\Device\Include\m0\platform_ctboard.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\HAL\Include\hal_gpio.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\Device\Include\reg_stm32f4xx.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\HAL\Include\hal_common.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\HAL\Include\hal_fmc.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\HAL\Include\hal_pwr.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\HAL\Include\hal_rcc.h \
+  C:\Users\mettl\AppData\Local\Arm\Packs\InES\CTBoard14_DFP\4.0.4\Device\Include\reg_ctboard.h
