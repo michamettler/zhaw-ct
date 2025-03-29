4a555d0be03058e2b695ab371ba7f273664dc8eb
mettlmi1 <mettlermicha@gmail.com>
2025-03-29 11:04:04 +0100

    finishd lab 5

--- CT2/Lab05_SP2/project/app/hal_spi.c
+++ CT2/Lab05_SP2/project/app/hal_spi.c
@@ -17,25 +17,19 @@
 #include "hal_spi.h"
 #include "hal_mocked.h"
 
-void hal_spi_init(void)
-{
-    hal_mocked_spi_init();
+void hal_spi_init(void) { hal_mocked_spi_init(); }
+
+uint8_t hal_spi_read_write(uint8_t send_byte) {
+  return hal_mocked_spi_read_write(send_byte);
 }
 
-
-uint8_t hal_spi_read_write(uint8_t send_byte)
-{
-    return hal_mocked_spi_read_write(send_byte);
-}
-
-
-#else  // !MOCKED_SPI_DISPLAY
-#include <reg_stm32f4xx.h>
+#else // !MOCKED_SPI_DISPLAY
 #include "hal_spi.h"
+#include <reg_stm32f4xx.h>
 
-#define BIT_TXE  (uint32_t)0x00000002
+#define BIT_TXE (uint32_t)0x00000002
 #define BIT_RXNE (uint32_t)0x00000001
-#define BIT_BSY  (uint32_t)0x00000080
+#define BIT_BSY (uint32_t)0x00000080
 
 static void set_ss_pin_low(void);
 static void set_ss_pin_high(void);
@@ -44,45 +38,57 @@ static void wait_10_us(void);
 /*
  * according to description in header file
  */
-void hal_spi_init(void)
-{
-    RCC->APB2ENR |= 0x00001000;     /**< enable SPI clock */
-    RCC->AHB1ENR |= 0x00000001;     /**< start clock on GPIO A */
-    GPIOA->OSPEEDR &= 0xFFFF00FF;   /**< clear P4 to P7 */
-    GPIOA->OSPEEDR |= 0x0000FF00;   /**< set P4 to P7 to 100 MHz */
-    GPIOA->MODER &= 0xFFFF00FF;     /**< clear mode on P5 to P7 */
-    /* P5 to P7, P4 output mode */
-    GPIOA->MODER |= 0x0000A900;     /**< Set alternate function mode on */
-    /* P5 to P7, P4 output mode */
-    GPIOA->AFRL &= 0x0000FFFF;      /**< clear alternate function */
-    GPIOA->AFRL |= 0x55550000;      /**< Set SPI1 alternate function */
+void hal_spi_init(void) {
+  RCC->APB2ENR |= 0x00001000;   /**< enable SPI clock */
+  RCC->AHB1ENR |= 0x00000001;   /**< start clock on GPIO A */
+  GPIOA->OSPEEDR &= 0xFFFF00FF; /**< clear P4 to P7 */
+  GPIOA->OSPEEDR |= 0x0000FF00; /**< set P4 to P7 to 100 MHz */
+  GPIOA->MODER &= 0xFFFF00FF;   /**< clear mode on P5 to P7 */
+  /* P5 to P7, P4 output mode */
+  GPIOA->MODER |= 0x0000A900; /**< Set alternate function mode on */
+  /* P5 to P7, P4 output mode */
+  GPIOA->AFRL &= 0x0000FFFF; /**< clear alternate function */
+  GPIOA->AFRL |= 0x55550000; /**< Set SPI1 alternate function */
 
-    SPI1->CR2 = 0x0000;             /**< set spi to default state */
-    SPI1->CR1 = 0x0000;             /**< set spi to default state */
+  SPI1->CR2 = 0x0000; /**< set spi to default state */
+  SPI1->CR1 = 0x0000; /**< set spi to default state */
 
-    /// STUDENTS: To be programmed
+  /// STUDENTS: To be programmed
 
+  SPI1->CR1 = 0x33C;
+  SPI1->CR1 |= 1 << 6; // SPE
 
-
-
-    /// END: To be programmed
-    set_ss_pin_high();
+  /// END: To be programmed
+  set_ss_pin_high();
 }
-
 
 /*
  * according to description in header file
  */
-uint8_t hal_spi_read_write(uint8_t send_byte)
-{
-    /// STUDENTS: To be programmed
+uint8_t hal_spi_read_write(uint8_t send_byte) {
+  /// STUDENTS: To be programmed
+  uint8_t receive_byte = 0x0;
 
+  set_ss_pin_low();
+  SPI1->DR = send_byte;
 
+  while ((SPI1->SR & BIT_TXE) == 0) {
+  }; // wait for txe flag
 
+  while ((SPI1->SR & BIT_RXNE) == 0) {
+  }; // wait for rxne flag
 
-    /// END: To be programmed
+  receive_byte = SPI1->DR;
+
+  while (((SPI1->SR & BIT_TXE) == 0) && ((SPI1->SR & BIT_BSY) != 0)) {
+  }; // wait for bsy flag
+
+  wait_10_us();
+  set_ss_pin_high();
+
+  return receive_byte;
+  /// END: To be programmed
 }
-
 
 /**
  * Set Slave-Select Pin (Port A, Pin 5) low
@@ -91,11 +97,9 @@ uint8_t hal_spi_read_write(uint8_t send_byte)
  *
  * No returns
  */
-static void set_ss_pin_low(void)
-{
-    GPIOA->BSRR |= 0x0010 << 16;    /**< Set Port A, Pin 5 low */
+static void set_ss_pin_low(void) {
+  GPIOA->BSRR |= 0x0010 << 16; /**< Set Port A, Pin 5 low */
 }
-
 
 /**
  * Set Slave-Select Pin (Port A, Pin 5) high
@@ -104,11 +108,9 @@ static void set_ss_pin_low(void)
  *
  * No returns
  */
-static void set_ss_pin_high(void)
-{
-    GPIOA->BSRR |= 0x0010;          /**< Set Port A, Pin 5 high */
+static void set_ss_pin_high(void) {
+  GPIOA->BSRR |= 0x0010; /**< Set Port A, Pin 5 high */
 }
-
 
 /**
  * Wait for approximately 10us
@@ -117,13 +119,11 @@ static void set_ss_pin_high(void)
  *
  * No returns
  */
-static void wait_10_us(void)
-{
-    uint8_t counter = 0;
-    while (counter < 160) {
-        counter++;
-    }
+static void wait_10_us(void) {
+  uint8_t counter = 0;
+  while (counter < 160) {
+    counter++;
+  }
 }
-
 
 #endif // MOCKED_SPI_DISPLAY
--- CT2/Lab05_SP2/project/app/lcd_io.c
+++ CT2/Lab05_SP2/project/app/lcd_io.c
@@ -16,14 +16,15 @@
  * ------------------------------------------------------------------
  */
 #include "lcd_io.h"
-#include "hal_spi.h"
 #include "hal_sbuf.h"
+#include "hal_spi.h"
+#include <stdint.h>
 
-#define ACK_CHAR         (uint8_t)0x06
-#define DC1_CHAR         (uint8_t)0x11
-#define DC2_CHAR         (uint8_t)0x12
-#define ESC_CHAR         (uint8_t)0x1B
-#define ONE_CHAR         (uint8_t)0x01
+#define ACK_CHAR (uint8_t)0x06
+#define DC1_CHAR (uint8_t)0x11
+#define DC2_CHAR (uint8_t)0x12
+#define ESC_CHAR (uint8_t)0x1B
+#define ONE_CHAR (uint8_t)0x01
 
 #define NOTHING_RECEIVED (uint8_t)0
 enum { SUCCESS = 0, ERRORCODE = 1 };
@@ -34,7 +35,6 @@ enum { SUCCESS = 0, ERRORCODE = 1 };
  */
 static uint8_t send_read_display_buffer_request(void);
 
-
 /* ------------------------------------------------------------------
  * -- Function implementations
  * ------------------------------------------------------------------
@@ -43,54 +43,97 @@ static uint8_t send_read_display_buffer_request(void);
 /*
  * according to description in header file
  */
-void init_display_interface(void)
-{
-    hal_spi_init();
-    hal_sbuf_init();
+void init_display_interface(void) {
+  hal_spi_init();
+  hal_sbuf_init();
 }
-
 
 /*
  * according to description in header file
  */
-uint8_t read_display_buffer(uint8_t *readBuffer)
-{
-    /// STUDENTS: To be programmed
+uint8_t read_display_buffer(uint8_t *readBuffer) {
+  /// STUDENTS: To be programmed
+  uint8_t dataAvailable = hal_sbuf_get_state();
+  if (dataAvailable == 0)
+    return 0; // no data available
+  
+  uint8_t ack = send_read_display_buffer_request();
+  if (ack != ACK_CHAR)
+    return 0; // no data available
 
 
+  // start read
+  uint8_t checksum = 0;
+  uint8_t dc1 = hal_spi_read_write(0x00);
+  checksum += dc1;
+  if (dc1 != DC1_CHAR) {
+    return 0;
+  }
 
+  // read string length
+  uint8_t length = hal_spi_read_write(0x00);
+  checksum += length;
 
-    /// END: To be programmed
+  // read string data
+  for (int i = 0; i < length; i++) {
+    uint8_t data = hal_spi_read_write(0x00);
+    readBuffer[i] = data;
+    checksum += data;
+  }
+
+  // check checksum
+  uint8_t receivedChecksum = hal_spi_read_write(0x00);
+  if (receivedChecksum != (checksum % 256))
+    return 0;
+
+  return length;
+  /// END: To be programmed
 }
-
 
 /*
  * according to description in header file
  */
-uint8_t write_cmd_to_display(const uint8_t *cmdBuffer, uint8_t length)
-{
-    /// STUDENTS: To be programmed
+uint8_t write_cmd_to_display(const uint8_t *cmdBuffer, uint8_t length) {
+  /// STUDENTS: To be programmed
 
+  uint8_t checksum = DC1_CHAR + ESC_CHAR + length + 1;
+  hal_spi_read_write(DC1_CHAR);
+  hal_spi_read_write(length + 1);
+  hal_spi_read_write(ESC_CHAR);
 
+  for (int i = 0; i < length; i++) { // before we did not have -1
+    hal_spi_read_write(cmdBuffer[i]);
+    checksum += cmdBuffer[i];
+  }
+  hal_spi_read_write(checksum % 256);
 
+  uint8_t result = hal_spi_read_write(0x00);
+  if (result == ACK_CHAR) {
+    return SUCCESS;
+  }
+  return ERRORCODE;
 
-    /// END: To be programmed
+  /// END: To be programmed
 }
-
 
 /*
  * Assemble and send a packet to trigger the reading of the display buffer
  * Uses the sequence "<DC2>, 0x01, 0x53, checksum" according to datasheet
  * Check if the ACK was sent by the lcd and return this
  */
-static uint8_t send_read_display_buffer_request(void)
-{
-    /// STUDENTS: To be programmed
+static uint8_t send_read_display_buffer_request(void) {
+  /// STUDENTS: To be programmed
+  uint8_t result;
+  // send read request
+  uint8_t checksum = DC2_CHAR + ONE_CHAR + 'S';
+  hal_spi_read_write(DC2_CHAR);
+  hal_spi_read_write(ONE_CHAR);
+  hal_spi_read_write('S');
+  hal_spi_read_write(checksum % 256);
 
+  // check if read req is ack
+  result = hal_spi_read_write(0x00);
 
-
-
-    /// END: To be programmed
-
+  return result;
+  /// END: To be programmed
 }
-
